9132a560c1f4e76fd065b3c2eec1eb0069959827
ohsg0226 <ohsg0226@naver.com>
2023-01-06 00:42:36 +0900

    feat: 전투기 선택 기능 추가

--- Assets/Scenes/SelectScript.cs
+++ Assets/Scenes/SelectScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a75654381bfcad9e3630811f72f132d428f775213eec881d11dbd73f5a8c335
-size 605
+oid sha256:c8b1c229235530a61f8778e072e9d5183ac98b109a3a4fc2f7b0f05102e51c01
+size 531
