12230c218bf7fdc327a406c0ef9fdbe9e73e520a
ohsg0226 <ohsg0226@naver.com>
2023-01-05 14:06:43 +0900

    Add 2D space kit

---
+++ Assets/2D Space Kit/Scripts/CustomPointer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42c14d847083dba14c893c9e90ffb9f09a129f94e890f4744be200401e8c9cf6
+size 5082
---
+++ Assets/2D Space Kit/Scripts/ExampleShipControl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:57e261fcf1b7c7083a29e8cd71556979742d5ba5d09d046565e40fa07831d544
+size 1870
