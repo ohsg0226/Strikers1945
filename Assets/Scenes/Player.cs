f71fa7fc6e6ab409e7f62ffbc52e18485a20a630
ohsg0226 <ohsg0226@naver.com>
2023-01-06 21:25:02 +0900

    feat: 총알이 움직일 수 있는 범위 설정

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:344ed3b0a5ede85b161c34066530ead8da863222cb2c9808952975c43713e1ba
-size 2144
+oid sha256:2ae97a188135f72837c09f0d3de537e1824de16028124ae9bbac7707e27ed5ca
+size 2190
