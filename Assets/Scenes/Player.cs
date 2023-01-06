db23210cf41d8f18e6c22519036a45c7bf65d10d
ohsg0226 <ohsg0226@naver.com>
2023-01-06 21:57:16 +0900

    feat: 총알 날아가는 속도 조절

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ae97a188135f72837c09f0d3de537e1824de16028124ae9bbac7707e27ed5ca
-size 2190
+oid sha256:52bb18314908bc6383c6b1bf3ef2f08fc925f360051da5668d174a7efeeb4456
+size 2767
