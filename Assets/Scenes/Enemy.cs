11ca50cc7f9c4f376b0936ace45a1038920189b2
ohsg0226 <ohsg0226@naver.com>
2023-01-07 18:15:52 +0900

    feat: 옆에서 나오는 적 비행기 추가

--- Assets/Scenes/Enemy.cs
+++ Assets/Scenes/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9bdb296acef000926ac28fc853f79949db747e7895b0361cd68b8a56b2b4864
-size 1097
+oid sha256:96e3c0704da2848ea06f21f80fd2f1b0d2d589f10ee73940f2f810bcc3365094
+size 869
--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b98647828f15e4c1d63e49aa6dd84b5c6fba6dd5cee03357b3f290ee500655ba
-size 895
+oid sha256:6a88a12704820ee9b7da92833094ff3cbbcb66c994fbdf5a39de5370aea8bb1a
+size 1456
