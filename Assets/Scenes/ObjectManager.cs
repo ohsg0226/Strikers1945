a32184c2acb7403233af45cedf71829a619acd7f
ohsg0226 <ohsg0226@naver.com>
2023-01-08 21:18:13 +0900

    feat 보조무기 추가

--- Assets/Scenes/ObjectManager.cs
+++ Assets/Scenes/ObjectManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:324329e3b92fb2353bc1d8396ac893e85a4e411b9686430fc62d0555ae044e01
-size 3886
+oid sha256:cb5e32e60a90b836b32efad7506b67424435c38ecaae77f1981a281f37ae2d70
+size 4416
--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6df9138235e391ec35f8ea2c5f0317f2727f7d94cf29e79081857a4106754eb
-size 5803
+oid sha256:7743609464ffc2c7b659c852fd64cf1ed5ada3db9c633e990bec5ab3f4a92238
+size 6130
