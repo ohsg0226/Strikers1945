7a4028ff0f7a6da6448a0d014dd6301dd8675eb0
ohsg0226 <ohsg0226@naver.com>
2023-01-09 17:51:07 +0900

    fix 플레이어가 죽었을 때 보조무기 안사라지는 문제 수정

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1037cf6f98671f1e4d0d83e58416e220e21d04af31cd49dbf1bdaad3b34d78df
-size 6678
+oid sha256:e89f316d5356cf62bd20d969cdc996821c7aeaa5cbbe8995b5568b242e492cb6
+size 6888
