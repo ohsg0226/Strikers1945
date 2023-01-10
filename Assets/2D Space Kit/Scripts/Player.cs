ea2e1d189933276d0f731389b0ab4839d6b67415
ohsg0226 <ohsg0226@naver.com>
2023-01-11 03:10:29 +0900

    feat: 미사일 발사, 필살기 발사 버튼 추가

--- Assets/2D Space Kit/Scripts/Player.cs
+++ Assets/2D Space Kit/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6052bf931218010eda04e3abd8d0b25cb1ade8b3222b4ed3ceb24183ceec9515
-size 7851
+oid sha256:9098bfd6362ad56b26f6f7a06e63f510bc403833216e4d02d1df2c5f1b37c85f
+size 8127
