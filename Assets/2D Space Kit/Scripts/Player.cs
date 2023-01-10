2b75fdc32d8ab07266c48747202dc90af8e8d458
ohsg0226 <ohsg0226@naver.com>
2023-01-11 01:00:23 +0900

    feat 조이스틱 추가

--- Assets/2D Space Kit/Scripts/Player.cs
+++ Assets/2D Space Kit/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e384669e9f074e3e998e968a39652ad04447a11b40fe12c5f9d523aa5012c77f
-size 7671
+oid sha256:6052bf931218010eda04e3abd8d0b25cb1ade8b3222b4ed3ceb24183ceec9515
+size 7851
